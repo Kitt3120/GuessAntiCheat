1342652c6c0568010e4c1dac464c0286fdbb4e25
Hamza <Hamza@Airstrike.Speedport_W_724V_01011603_00_005>
2017-05-14 00:45:20 +0200

    Added Waterwalk

---
+++ src/de/kitt3120/guessanticheat/modules/modules/WaterWalk.java
@@ -0,0 +1,37 @@
+package de.kitt3120.guessanticheat.modules.modules;
+
+import org.bukkit.plugin.Plugin;
+
+import de.kitt3120.guessanticheat.modules.Module;
+
+public class WaterWalk extends Module{
+
+	public WaterWalk(Plugin plugin, String name, String description, boolean enableOnRegister) {
+		super(plugin, name, description, enableOnRegister);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onRegister() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTick() {
+		// TODO Auto-generated method stub
+		
+	}
+}
