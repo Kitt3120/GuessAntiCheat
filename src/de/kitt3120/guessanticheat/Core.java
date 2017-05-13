0315355e1548ac767228a8ae5c36bee0f8d4b016
kitt3120 <Kitt3120@aol.de>
2017-05-13 23:20:26 +0200

    Initial commit

---
+++ src/de/kitt3120/guessanticheat/Core.java
@@ -0,0 +1,14 @@
+package de.kitt3120.guessanticheat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by kitt3120 on 13.05.2017 at 23:19.
+ */
+public class Core extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+}
